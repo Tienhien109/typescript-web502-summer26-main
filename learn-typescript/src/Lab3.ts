9f2ac19fa2d52f7efad5ff9ce707d75324dcc482
Tienhien109 <you@example.com>
2026-07-13 18:09:29 +0700

    lab3bai1

---
+++ learn-typescript/src/Lab3.ts
@@ -0,0 +1,11 @@
+//bai 1
+const averageScore = (scores: number[]): number => {
+    let sum = 0;
+    for (let score of scores) {
+        sum += score;
+    }
+    return sum / scores.length;
+}
+console.log(averageScore([8, 9, 10]));
+console.log(averageScore([5, 6, 7, 8]));
+
