b5c1d3a6f212b8253e36b389d9aa758f58a015b3
Tienhien109 <you@example.com>
2026-07-15 17:27:18 +0700

    bai2lab4

--- learn-typescript/src/Lab4.ts
+++ learn-typescript/src/Lab4.ts
@@ -13,3 +13,23 @@ console.log(`Name: ${this.name}`);
 }
 const student1 = new Student(1, "Hiennt");
 student1.display();
+
+//bai2
+interface User {
+    id: number;
+    email: string;
+    phone?: string;
+}
+
+const user1: User = {
+    id: 1,
+    email: "hiennt@gmail.com",
+    phone: "0332405972"
+};
+const user2: User = {
+    id: 2,
+    email: "hien22@gmail.com",
+    phone: "0332405972"
+};
+console.log(user1);
+console.log(user2);
