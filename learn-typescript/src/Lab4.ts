06238111ba1662dac215442ac940cf57c6466bd0
Tienhien109 <you@example.com>
2026-07-15 17:36:01 +0700

    bai5lab4

--- learn-typescript/src/Lab4.ts
+++ learn-typescript/src/Lab4.ts
@@ -69,4 +69,12 @@ logStatus("loading");
 logStatus("success");
 logStatus("error");
 
+//bai5
+function identity<T>(value: T): T {
+    return value;
+}
+
+console.log(identity<number>(10));
+console.log(identity<string>("Hello"));
+console.log(identity<boolean>(true));
 
