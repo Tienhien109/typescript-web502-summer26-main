a9c9cc4cf755962ce4002c061d1a098d1e272d04
Tienhien109 <you@example.com>
2026-07-15 17:09:53 +0700

    update lesson 4

---
+++ learn-typescript/src/Lesson4.ts
@@ -0,0 +1,50 @@
+class person {
+    // property
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+}
+greet() {
+    console.log(`Hello, my name is ${this.name}`);
+}
+}   
+const person1 = new person("Hien");
+person1.greet();
+
+interface Student {
+    name: string;
+    age: number;
+    isDone?: boolean;
+}
+
+const s2: Student = {
+    name: "Hiennt",
+    age: 20,
+    isDone: true
+};
+
+console.log(s2);
+
+type Product = {
+    title: string;
+    price: number;
+}
+type status = "success" | "error";
+const statusSuccess: status = "success";
+
+type Person = {
+    name: string;
+};
+
+type Employee = {
+    id: string;
+};
+
+type EmployeeInfo = Person & Employee;
+
+enum Role {
+    ADMIN = "admin",
+    USER = "user",
+}
+
+console.log(Role.USER);
